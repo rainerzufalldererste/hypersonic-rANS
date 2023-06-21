d37abd84b9de48eaf3fb8d2721349c61f04dba87
Christoph Stiller <c.stiller@live.de>
2023-06-21 19:16:12 +0200

    StdDev + Avg

--- src/rans.cpp
+++ src/rans.cpp
@@ -22,6 +22,42 @@ constexpr size_t EncodeEmitPoint = ((DecodeConsumePoint >> TotalSymbolCountBits)
 
 static_assert(TotalSymbolCountBits <= 16);
 
+constexpr size_t RunCount = 10;
+static uint64_t _ClocksPerRun[RunCount];
+static uint64_t _NsPerRun[RunCount];
+
+void print_perf_info(const size_t fileSize)
+{
+  uint64_t completeNs = 0;
+  uint64_t completeClocks = 0;
+
+  for (size_t i = 0; i < RunCount; i++)
+  {
+    completeNs += _NsPerRun[i];
+    completeClocks += _ClocksPerRun[i];
+  }
+
+  const double meanNs = completeNs / (double)RunCount;
+  const double meanClocks = completeClocks / (double)RunCount;
+  double stdDevNs = 0;
+  double stdDevClocks = 0;
+
+  for (size_t i = 0; i < RunCount; i++)
+  {
+    const double diffNs = _NsPerRun[i] - meanNs;
+    const double diffClocks = _ClocksPerRun[i] - meanClocks;
+
+    stdDevNs += diffNs * diffNs;
+    stdDevClocks += diffClocks * diffClocks;
+  }
+
+  stdDevNs = sqrt(stdDevNs / (double)(RunCount - 1));
+  stdDevClocks = sqrt(stdDevClocks / (double)(RunCount - 1));
+
+  printf("\nAverage: \t%5.3f clk/byte\t(std dev: %5.3f ~ %5.3f)\n", meanClocks / fileSize, (meanClocks - stdDevClocks) / fileSize, (meanClocks + stdDevClocks) / fileSize);
+  printf("Average: \t%5.3f MiB/s\t(std dev: %5.3f ~ %5.3f)\n\n", (fileSize / (1024.0 * 1024.0)) / (meanNs * 1e-9), (fileSize / (1024.0 * 1024.0)) / ((meanNs + stdDevNs) * 1e-9), (fileSize / (1024.0 * 1024.0)) / ((meanNs - stdDevNs) * 1e-9));
+}
+
 struct hist_t
 {
   uint16_t symbolCount[256];
@@ -56,7 +92,7 @@ void make_hist(hist_t *pHist, const uint8_t *pData, const size_t size)
   uint16_t capped[256];
   size_t cappedSum = 0;
 
-  constexpr bool FloatingPointHistLimit = false;
+  constexpr bool FloatingPointHistLimit = true;
 
   if constexpr (FloatingPointHistLimit)
   {
@@ -64,7 +100,11 @@ void make_hist(hist_t *pHist, const uint8_t *pData, const size_t size)
 
     for (size_t i = 0; i < 256; i++)
     {
-      capped[i] = (uint16_t)((uint32_t)(hist[i] * mul + 0.5f) | (uint32_t)!!(hist[i]));
+      capped[i] = (uint16_t)(hist[i] * mul + 0.5f);
+
+      if (capped[i] == 0 && hist[i])
+        capped[i] = 1;
+
       cappedSum += capped[i];
     }
   }
@@ -78,7 +118,11 @@ void make_hist(hist_t *pHist, const uint8_t *pData, const size_t size)
 
       for (size_t i = 0; i < 256; i++)
       {
-        capped[i] = (uint16_t)((uint32_t)(((hist[i] + add) / div) | (uint32_t)!!(hist[i])));
+        capped[i] = (uint16_t)((hist[i] + add) / div);
+
+        if (capped[i] == 0 && hist[i])
+          capped[i] = 1;
+
         cappedSum += capped[i];
       }
     }
@@ -442,32 +486,50 @@ int32_t main(const int64_t argc, char **pArgv)
 
   size_t compressedLength = 0;
 
-  for (size_t run = 0; run < 10; run++)
   {
-    const uint64_t startTick = GetCurrentTimeTicks();
-    const uint64_t startClock = __rdtsc();
     compressedLength = encode_basic(pUncompressedData, fileSize, pCompressedData, compressedDataCapacity, &hist);
-    const uint64_t endClock = __rdtsc();
-    const uint64_t endTick = GetCurrentTimeTicks();
 
-    _mm_mfence();
+    for (size_t run = 0; run < RunCount; run++)
+    {
+      const uint64_t startTick = GetCurrentTimeTicks();
+      const uint64_t startClock = __rdtsc();
+      compressedLength = encode_basic(pUncompressedData, fileSize, pCompressedData, compressedDataCapacity, &hist);
+      const uint64_t endClock = __rdtsc();
+      const uint64_t endTick = GetCurrentTimeTicks();
 
-    printf("encode_basic: \t%" PRIu64 " bytes from %" PRIu64 " bytes. (%5.3f %%, %6.3f clocks/byte, %5.2f MiB/s)\n", compressedLength, fileSize, compressedLength / (double)fileSize * 100.0, (endClock - startClock) / (double)fileSize, (fileSize / (1024.0 * 1024.0)) / (TicksToNs(endTick - startTick) * 1e-9));
+      _mm_mfence();
+
+      _NsPerRun[run] = TicksToNs(endTick - startTick);
+      _ClocksPerRun[run] = endClock - startClock;
+
+      printf("encode_basic: \t%" PRIu64 " bytes from %" PRIu64 " bytes. (%5.3f %%, %6.3f clocks/byte, %5.2f MiB/s)\n", compressedLength, fileSize, compressedLength / (double)fileSize * 100.0, (endClock - startClock) / (double)fileSize, (fileSize / (1024.0 * 1024.0)) / (TicksToNs(endTick - startTick) * 1e-9));
+    }
+
+    print_perf_info(fileSize);
   }
 
   puts("");
 
-  for (size_t run = 0; run < 10; run++)
   {
-    const uint64_t startTick = GetCurrentTimeTicks();
-    const uint64_t startClock = __rdtsc();
     compressedLength = encode(pUncompressedData, fileSize, pCompressedData, compressedDataCapacity, &histEnc);
-    const uint64_t endClock = __rdtsc();
-    const uint64_t endTick = GetCurrentTimeTicks();
 
-    _mm_mfence();
+    for (size_t run = 0; run < RunCount; run++)
+    {
+      const uint64_t startTick = GetCurrentTimeTicks();
+      const uint64_t startClock = __rdtsc();
+      compressedLength = encode(pUncompressedData, fileSize, pCompressedData, compressedDataCapacity, &histEnc);
+      const uint64_t endClock = __rdtsc();
+      const uint64_t endTick = GetCurrentTimeTicks();
 
-    printf("encode: \t%" PRIu64 " bytes from %" PRIu64 " bytes. (%5.3f %%, %6.3f clocks/byte, %5.2f MiB/s)\n", compressedLength, fileSize, compressedLength / (double)fileSize * 100.0, (endClock - startClock) / (double)fileSize, (fileSize / (1024.0 * 1024.0)) / (TicksToNs(endTick - startTick) * 1e-9));
+      _mm_mfence();
+
+      _NsPerRun[run] = TicksToNs(endTick - startTick);
+      _ClocksPerRun[run] = endClock - startClock;
+
+      printf("encode: \t%" PRIu64 " bytes from %" PRIu64 " bytes. (%5.3f %%, %6.3f clocks/byte, %5.2f MiB/s)\n", compressedLength, fileSize, compressedLength / (double)fileSize * 100.0, (endClock - startClock) / (double)fileSize, (fileSize / (1024.0 * 1024.0)) / (TicksToNs(endTick - startTick) * 1e-9));
+    }
+
+    print_perf_info(fileSize);
   }
 
   puts("");
@@ -518,32 +580,50 @@ int32_t main(const int64_t argc, char **pArgv)
 
   size_t decompressedLength = 0;
 
-  for (size_t run = 0; run < 10; run++)
   {
-    const uint64_t startTick = GetCurrentTimeTicks();
-    const uint64_t startClock = __rdtsc();
     decompressedLength = decode_basic(pCompressedData, compressedLength, pDecompressedData, fileSize, &histDec);
-    const uint64_t endClock = __rdtsc();
-    const uint64_t endTick = GetCurrentTimeTicks();
 
-    _mm_mfence();
+    for (size_t run = 0; run < RunCount; run++)
+    {
+      const uint64_t startTick = GetCurrentTimeTicks();
+      const uint64_t startClock = __rdtsc();
+      decompressedLength = decode_basic(pCompressedData, compressedLength, pDecompressedData, fileSize, &histDec);
+      const uint64_t endClock = __rdtsc();
+      const uint64_t endTick = GetCurrentTimeTicks();
 
-    printf("decode_basic: \tdecompressed to %" PRIu64 " bytes (should be %" PRIu64 "). (%6.3f clocks/byte, %5.2f MiB/s)\n", decompressedLength, fileSize, (endClock - startClock) / (double)fileSize, (fileSize / (1024.0 * 1024.0)) / (TicksToNs(endTick - startTick) * 1e-9));
+      _mm_mfence();
+
+      _NsPerRun[run] = TicksToNs(endTick - startTick);
+      _ClocksPerRun[run] = endClock - startClock;
+
+      printf("decode_basic: \tdecompressed to %" PRIu64 " bytes (should be %" PRIu64 "). (%6.3f clocks/byte, %5.2f MiB/s)\n", decompressedLength, fileSize, (endClock - startClock) / (double)fileSize, (fileSize / (1024.0 * 1024.0)) / (TicksToNs(endTick - startTick) * 1e-9));
+    }
+
+    print_perf_info(fileSize);
   }
 
   puts("");
 
-  for (size_t run = 0; run < 10; run++)
   {
-    const uint64_t startTick = GetCurrentTimeTicks();
-    const uint64_t startClock = __rdtsc();
     decompressedLength = decode(pCompressedData, compressedLength, pDecompressedData, fileSize, &histDec);
-    const uint64_t endClock = __rdtsc();
-    const uint64_t endTick = GetCurrentTimeTicks();
 
-    _mm_mfence();
+    for (size_t run = 0; run < RunCount; run++)
+    {
+      const uint64_t startTick = GetCurrentTimeTicks();
+      const uint64_t startClock = __rdtsc();
+      decompressedLength = decode(pCompressedData, compressedLength, pDecompressedData, fileSize, &histDec);
+      const uint64_t endClock = __rdtsc();
+      const uint64_t endTick = GetCurrentTimeTicks();
 
-    printf("decode: \tdecompressed to %" PRIu64 " bytes (should be %" PRIu64 "). (%6.3f clocks/byte, %5.2f MiB/s)\n", decompressedLength, fileSize, (endClock - startClock) / (double)fileSize, (fileSize / (1024.0 * 1024.0)) / (TicksToNs(endTick - startTick) * 1e-9));
+      _mm_mfence();
+
+      _NsPerRun[run] = TicksToNs(endTick - startTick);
+      _ClocksPerRun[run] = endClock - startClock;
+
+      printf("decode: \tdecompressed to %" PRIu64 " bytes (should be %" PRIu64 "). (%6.3f clocks/byte, %5.2f MiB/s)\n", decompressedLength, fileSize, (endClock - startClock) / (double)fileSize, (fileSize / (1024.0 * 1024.0)) / (TicksToNs(endTick - startTick) * 1e-9));
+    }
+
+    print_perf_info(fileSize);
   }
 
   puts("");
