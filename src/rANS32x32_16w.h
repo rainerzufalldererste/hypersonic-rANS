9269f3bdb657860c4318086ee1ce899555343183
Christoph Stiller <c.stiller@live.de>
2023-07-03 04:40:12 +0200

    how isn't this fast, zen4???

--- src/rANS32x32_16w.h
+++ src/rANS32x32_16w.h
@@ -95,4 +95,15 @@ size_t rANS32x32_xmmShfl2_16w_decode_avx512_ymmGthr_varC_10(const uint8_t *pInDa
 size_t rANS32x32_ymmShfl_16w_decode_avx512_ymmGthr_varC_10(const uint8_t *pInData, const size_t inLength, uint8_t *pOutData, const size_t outCapacity);
 size_t rANS32x32_ymmShfl2_16w_decode_avx512_ymmGthr_varC_10(const uint8_t *pInData, const size_t inLength, uint8_t *pOutData, const size_t outCapacity);
 
+size_t rANS32x32_xmmShfl_16w_decode_avx2_pregather_varC_12(const uint8_t *pInData, const size_t inLength, uint8_t *pOutData, const size_t outCapacity);
+size_t rANS32x32_xmmShfl_16w_decode_avx2_pregather_varC_11(const uint8_t *pInData, const size_t inLength, uint8_t *pOutData, const size_t outCapacity);
+size_t rANS32x32_xmmShfl_16w_decode_avx2_pregather_varC_10(const uint8_t *pInData, const size_t inLength, uint8_t *pOutData, const size_t outCapacity);
+
+size_t rANS32x32_xmmShfl2_16w_decode_avx2_pregather_varC_12(const uint8_t *pInData, const size_t inLength, uint8_t *pOutData, const size_t outCapacity);
+size_t rANS32x32_xmmShfl2_16w_decode_avx2_pregather_varC_11(const uint8_t *pInData, const size_t inLength, uint8_t *pOutData, const size_t outCapacity);
+size_t rANS32x32_xmmShfl2_16w_decode_avx2_pregather_varC_10(const uint8_t *pInData, const size_t inLength, uint8_t *pOutData, const size_t outCapacity);
+
+size_t rANS32x32_ymmPerm_16w_decode_avx2_pregather_varC_12(const uint8_t *pInData, const size_t inLength, uint8_t *pOutData, const size_t outCapacity);
+size_t rANS32x32_ymmPerm_16w_decode_avx2_pregather_varC_11(const uint8_t *pInData, const size_t inLength, uint8_t *pOutData, const size_t outCapacity);
+size_t rANS32x32_ymmPerm_16w_decode_avx2_pregather_varC_10(const uint8_t *pInData, const size_t inLength, uint8_t *pOutData, const size_t outCapacity);
 #endif // rANS32x32_16w_h__
